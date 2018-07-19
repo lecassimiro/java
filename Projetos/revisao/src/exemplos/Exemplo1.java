a44b9dd61a7106a6ef76b8095f1e0da0d3979c0b
LEANDRO MARCELO CASSIMIRO DAS CHAGAS <a9244919@rn.senac.br>
2018-07-18 21:15:17 -0300

    commit inicial

---
+++ Projetos/revisao/src/exemplos/Exemplo1.java
@@ -0,0 +1,32 @@
+package exemplos;
+
+public class Exemplo1 {
+	
+	public static void main(String args[]) {
+		
+		//tipo primitivo de numeros inteiros
+		int numero1 = 30;
+		long numero2 = 10L;
+		
+		//tipo primitivo de numeros decimais
+		float numero3 = 35.5f;
+		double numero4 = 51.2;
+		
+		//tipo primitivo de caractere
+		boolean ehMaior = true;
+		
+		//tipo primitivo logico
+		char letra = 'b';
+		
+		//Classe que representa as strings
+		String nome = "Weskley";
+		
+		int soma = numero1 +(int) numero2;
+		
+		System.out.println();
+		
+		System.out.println(soma);
+
+	}
+
+}
